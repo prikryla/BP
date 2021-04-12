a36eb899c44cc24962ad873f43021a325d9f536e
Adam Prikryl <adam.prikryl@biostatistika.cz>
2021-04-13 00:11:28 +0200

    New migration version (updates with constraints)

---
+++ migrations/Version20210412205820.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210412205820 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE matches DROP match_date');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE matches ADD match_date DATE NOT NULL');
+        $this->addSql('COMMENT ON COLUMN matches.match_date IS \'match_date\'');
+    }
+}
